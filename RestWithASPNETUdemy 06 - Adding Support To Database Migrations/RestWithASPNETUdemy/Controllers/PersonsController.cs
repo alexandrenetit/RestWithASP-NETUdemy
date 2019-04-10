92498ee2c5d2483165c44a02c74f9ab2fe812231
Alexandre Gonçalves <alexandre.goncalves@farfetch.com>
2019-04-10 13:12:20 +0100

    Add session 06 migrations

---
+++ RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Business;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonBusiness _personBusiness;
+
+        public PersonsController(IPersonBusiness personService)
+        {
+            _personBusiness = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personBusiness.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personBusiness.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
