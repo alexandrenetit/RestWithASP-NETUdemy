2d5cc57c7e6d5b79d381134310f7cd31b8f798c5
Alexandre Gonçalves <alexandre.goncalves@farfetch.com>
2019-05-15 11:54:02 +0100

    add HATEOAS

---
+++ RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImpl.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASPNETUdemy.Data.Converters;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+        private IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImpl(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return  _converter.ParseList(_repository.FindAll());
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public bool Exists(long id)
+        {
+            return _repository.Exists(id);
+        }
+    }
+}
---
+++ RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using Tapioca.HATEOAS;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonBusiness _personBusiness;
+
+        public PersonsController(IPersonBusiness personService)
+        {
+            _personBusiness = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public ActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public ActionResult Get(int id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public ActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personBusiness.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public ActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            var updatedPerson = _personBusiness.Update(person);
+
+            if (updatedPerson == null)
+                return BadRequest();
+
+            return new ObjectResult(updatedPerson);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
